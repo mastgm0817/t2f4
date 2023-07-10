c8d97e22f8c902a00a6a8248e09b107c5f808daa
rhquddus <rhquddus@naver.com>
2023-07-10 14:00:25 +0900

    code

---
+++ src/main/java/com/example/t2f4/T2f4Application.java
@@ -0,0 +1,28 @@
+package com.example.t2f4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class T2f4Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "welcome to myworld";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(T2f4Application.class, args);
+	}
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/mastgm0817/t2f4.git
+// git push -u origin main
+
+}
---
+++ src/test/java/com/example/t2f4/T2f4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t2f4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2f4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
